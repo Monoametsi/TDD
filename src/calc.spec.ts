1a7e7e3b1bbe134a853743afa9f4dd2b3c4751f9
Monoametsi Mosemeng <monoametsi@ikhokha.com>
2022-08-13 14:30:46 +0200

    add subtract multiply divide power of root of test

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -5,23 +5,23 @@ describe('Calc', () => {
         expect(calc.add(6, 4)).toBe(10)
     })
 
-    test('should return 2 for subtract(6, 4)', () => {
+    test('should return 10 for subtract(6, 4)', () => {
         expect(calc.subtract(6, 4)).toBe(2)
     })
 
-    // test('should return 5 for add(6, -1)', () => {
-    //     expect(calc.add(6, -1)).toBe(5)
-    // })
+    test('should return 10 for multiply(5, 2)', () => {
+        expect(calc.multiply(5, 2)).toBe(10)
+    })
 
-    // test('should return 10 for add(6, 4)', () => {
-    //     expect(calc.add(6, 4)).toBe(10)
-    // })
+    test('should return 10 for divide(20, 2)', () => {
+        expect(calc.divide(20, 2)).toBe(10)
+    })
 
-    // test('should return 5 for add(6, -1)', () => {
-    //     expect(calc.add(6, -1)).toBe(5)
-    // })
+    test('should return 25 for powerOf(5, 2)', () => {
+        expect(calc.powerOf(5, 2)).toBe(25)
+    })
 
-    // test('should return 10 for add(6, 4)', () => {
-    //     expect(calc.add(6, 4)).toBe(10)
-    // })
+    test('should return 5 for rootOf(25)', () => {
+        expect(calc.rootOf(25)).toBe(5)
+    })
 })
